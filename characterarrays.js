787c81629e9dcec5bcb73203bf4f5dddcc272c08
narani-11 <naranitejaswini29@gmail.com>
2025-07-12 20:05:53 -0500

    Create characterarrays.js

---
+++ characterarrays.js
@@ -0,0 +1,19 @@
+// Task 1: Decode the reversed messages
+let msg1 = " !yako eb ll'uoy dna ,gniog peeK !sgnittes fo yteirav a ni slliks gnidoc esu osla nac uoY !ti teg ll'uoy ,tsisrep uoy fi tub ,tsrif ta drah mees yam gnidoC";
+let msg2 = "!ecitcarp htiw retteb teg ll‘uoy ,emit ekaT .tsrif ta drah leef lliw gnitirw edoc tuB";
+let msg3 = "!elpoep rehto morf tnereffid on era uoy ,elbuort evah uoy fI .lanoisseforp a ekil leef ot evah t'nod uoY";
+let msg4 = ".rettam llits yeht ,smargorp llams etirw ylno nac uoy fI .tnemom tcefrep eht rof tiaw t'noD .yadot trats tsuJ";
+
+console.log(msg1.split('').reverse().join(''));
+console.log(msg2.split('').reverse().join(''));
+console.log(msg3.split('').reverse().join(''));
+console.log(msg4.split('').reverse().join(''));
+
+// Task 2: Write your own reverse messages
+let myMsg1 = "You can learn anything if you try.";
+let myMsg2 = "Keep moving forward.";
+let myMsg3 = "Small steps lead to big changes.";
+
+console.log(myMsg1.split('').reverse().join(''));
+console.log(myMsg2.split('').reverse().join(''));
+console.log(myMsg3.split('').reverse().join(''));
